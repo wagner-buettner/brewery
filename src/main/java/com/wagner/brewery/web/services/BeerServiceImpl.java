5a2e39e1abe886bdaf0824723bf748def6c8d387
Wagner Morais Domingues <wagner.morais@gmail.com>
2019-12-16 15:06:28 +0100

    4.34 Delete Method for BeerController

--- src/main/java/com/wagner/brewery/web/services/BeerServiceImpl.java
+++ src/main/java/com/wagner/brewery/web/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.wagner.brewery.web.services;
 
 import com.wagner.brewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -25,5 +27,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deteleById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
