fae7e852094a4eb53a400c7c9e1bda17706e6b16
Wagner Morais Domingues <wagner.morais@gmail.com>
2019-12-16 12:08:27 +0100

    4.32 PUT Mapping for BeerController

--- src/main/java/com/wagner/brewery/web/services/BeerServiceImpl.java
+++ src/main/java/com/wagner/brewery/web/services/BeerServiceImpl.java
@@ -21,4 +21,9 @@ public class BeerServiceImpl implements BeerService {
                 .id(UUID.randomUUID())
                 .build();
     }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        //todo impl - would add a real impl to update beer
+    }
 }
