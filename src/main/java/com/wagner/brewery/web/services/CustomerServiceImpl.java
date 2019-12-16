60fcb4048edb21f7c3f992b01beefa1cceeec89e
Wagner Morais Domingues <wagner.morais@gmail.com>
2019-12-16 15:39:20 +0100

    4.36 Post, Put and Delete for CustomerController

--- src/main/java/com/wagner/brewery/web/services/CustomerServiceImpl.java
+++ src/main/java/com/wagner/brewery/web/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.wagner.brewery.web.services;
 
 import com.wagner.brewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -12,5 +14,24 @@ public class CustomerServiceImpl implements CustomerService {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("Wagner Morais Domingues")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl
+        log.debug("Updating...");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        //todo impl
+        log.debug("Deleting...");
     }
 }
