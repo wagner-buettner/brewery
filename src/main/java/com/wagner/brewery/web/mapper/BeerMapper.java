d6a9dcc1398ecc21d016e88c660d5c4868d9f66c
Wagner Morais Domingues <wagner.morais@gmail.com>
2020-01-15 11:24:41 +0100

    81. Date Conversion with Mapstruct

--- src/main/java/com/wagner/brewery/web/mapper/BeerMapper.java
+++ src/main/java/com/wagner/brewery/web/mapper/BeerMapper.java
@@ -4,7 +4,7 @@ import com.wagner.brewery.domain.Beer;
 import com.wagner.brewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
